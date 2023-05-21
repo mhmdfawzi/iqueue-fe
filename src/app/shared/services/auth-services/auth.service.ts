698abc42c6c1f1afd79d5ce3349e352fe77228fc
Mohamed Fawzy <mohamed.fawzy@qeema.net>
2023-05-21 14:45:23 +0300

    --

--- src/app/shared/services/auth-services/auth.service.ts
+++ src/app/shared/services/auth-services/auth.service.ts
@@ -10,35 +10,37 @@ import { CookieService } from 'ngx-cookie-service';
 import { HttpResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 
-export interface ResponseLogin{
-  success: boolean,
-  message: string,
-  data: LoggedUser
+export interface ResponseLogin {
+  success: boolean;
+  message: string;
+  data: LoggedUser;
 }
 
 export interface LoggedUser {
-  username: string,
-  role: string,
-  id: string
+  username: string;
+  role: string;
+  id: string;
 }
 
 @Injectable({
-   providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   //  isUserLoggedIn: boolean = false;
-  loggedInUser : BehaviorSubject<LoggedUser | null> = new BehaviorSubject<LoggedUser | null>(null);
+  loggedInUser: BehaviorSubject<LoggedUser | null> =
+    new BehaviorSubject<LoggedUser | null>(null);
   isLogged: boolean = false;
 
-   constructor(
+  constructor(
     private generalService: GeneralService,
-    private router: Router
-    // private cookieService: CookieService
+    private router: Router // private cookieService: CookieService
   ) {}
 
-  loginUser(loginForm: LoginForm): Observable<ResponseLogin>{
-    return this.generalService.postAPIData<ResponseLogin>(`${environment.apiUrlAuth}/login`, loginForm)
+  loginUser(loginForm: LoginForm): Observable<ResponseLogin> {
+    return this.generalService.postAPIData<ResponseLogin>(
+      `${environment.apiUrlAuth}/login`,
+      loginForm
+    );
   }
 
   // loginUser(loginForm: LoginForm){
@@ -46,43 +48,41 @@ export class AuthService {
   // }
 
   handleSuccessfulLogin(user: LoggedUser) {
-    // user.role = "owner"
+    user.role = 'manager';
     this.loggedInUser.next(user);
     this.isLogged = true;
 
-    this.router.navigate(['home'])
+    this.router.navigate(['home']);
     // const expiresAt = moment().add(authResult.expiresIn,'second');
 
     // localStorage.setItem('id_token', authResult.idToken);
     // localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()) );
   }
 
-  registerUser(formData: RegisterForm){
-    if(!formData.role){ // *This is to set role by default to basic if not passed already .
-      formData.role = 'basic'
+  registerUser(formData: RegisterForm) {
+    if (!formData.role) {
+      // *This is to set role by default to basic if not passed already .
+      formData.role = 'basic';
     }
 
-    return this.generalService.postAPIData(`${environment.apiUrlAuth}/register`, formData)
+    return this.generalService.postAPIData(
+      `${environment.apiUrlAuth}/register`,
+      formData
+    );
   }
 
   logout() {
-    localStorage.removeItem("id_token");
-    localStorage.removeItem("expires_at");
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
   }
-
-
 
   // isLoggedOut() {
   //   return !this.isLoggedIn();
   // }
-
-
 
   // getExpiration() {
   //   const expiration : any = localStorage.getItem("expires_at");
   //   const expiresAt = JSON.parse(expiration);
   //   return moment(expiresAt);
   // }
-
-
 }
